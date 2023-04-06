7e011a29171f45e6dd742dd18b6cc3419df1deb2
David Shnayder <dusrdev@outlook.com>
2023-04-06 13:52:51 +0300

    Updated and added tests

---
+++ Sharpify.Tests/DateAndTimeExtensions.cs
@@ -0,0 +1,59 @@
+namespace Sharpify.Tests;
+
+public class DateAndTimeExtensions {
+    [Theory]
+    [InlineData(0.00001, "0.01ms")]
+    [InlineData(0.01, "0.01ms")]
+    [InlineData(0.5, "0.5s")]
+    [InlineData(1, "1s")]
+    [InlineData(59.999, "59.99s")]
+    [InlineData(60, "1m")]
+    [InlineData(61, "1.02m")]
+    [InlineData(121.234, "2.02m")]
+    public void Format_GivenTimeSpan_ReturnsFormattedString(double seconds, string expected) {
+        // Arrange
+        var elapsed = TimeSpan.FromSeconds(seconds);
+
+        // Act
+        var result = elapsed.Format();
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void ToRemainingDuration_GivenTimeSpan_ReturnsHumanReadableFormat() {
+        // Arrange
+        var time = new TimeSpan(1, 2, 3, 4, 567);
+
+        // Act
+        var result = time.ToRemainingDuration();
+
+        // Assert
+        result.Should().Be("1d 2h 3m 4s");
+    }
+
+    [Fact]
+    public void ToRemainingDuration_GivenTimeSpanLessThanOneSecond_ReturnsZeroSeconds() {
+        // Arrange
+        var time = TimeSpan.FromMilliseconds(500);
+
+        // Act
+        var result = time.ToRemainingDuration();
+
+        // Assert
+        result.Should().Be("0s");
+    }
+
+    [Fact]
+    public void ToTimeStamp_GivenDateTime_ReturnsTimeStamp() {
+        // Arrange
+        var dateTime = new DateTime(2022, 04, 06, 13, 55, 00);
+
+        // Act
+        var result = dateTime.ToTimeStamp();
+
+        // Assert
+        result.Should().Be("1355-06-Apr-22");
+    }
+}
--- Sharpify.Tests/Usings.cs
+++ Sharpify.Tests/Usings.cs
@@ -1,11 +1,11 @@
-global using System.Runtime.InteropServices;
-
-global using System.Runtime.CompilerServices;
-
 global using System.Collections.Generic;
+global using System.Runtime.CompilerServices;
+global using System.Runtime.InteropServices;
 
 global using FluentAssertions;
 
 global using Sharpify;
 
 global using Xunit;
+
+global using static Sharpify.Utils;
