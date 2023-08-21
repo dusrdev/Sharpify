24fcebb57444b88f7ec4f52f0f7c46c654ea7cd3
David Shnayder <dusrdev@outlook.com>
2023-08-21 15:48:47 +0300

    Added chunks and improved performance

--- Sharpify/CollectionExtensions.cs
+++ Sharpify/CollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -55,6 +56,9 @@ public static partial class Extensions {
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public static T[] PureSort<T>(this T[] source, IComparer<T> comparer) {
+        if (source.Length <= 1) {
+            return source;
+        }
         var newArr = new T[source.Length];
         Array.Copy(source, newArr, source.Length);
         Array.Sort(newArr, comparer);
@@ -74,29 +78,38 @@ public static partial class Extensions {
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public static List<T> PureSort<T>(this IEnumerable<T> source, IComparer<T> comparer) {
         var list = new List<T>(source);
+        if (list.Count <= 1) {
+            return list;
+        }
         list.Sort(comparer);
         return list;
     }
 
     /// <summary>
-    /// Removes duplicates for a sorted list
+    /// Removes duplicates from a list. If <paramref name="isSorted"/> is true, the list is assumed to be sorted and duplicates are removed in one iteration without allocating an additional collection. Otherwise, a HashSet is used to remove duplicates in one iteration. An optional <paramref name="comparer"/> can be provided to compare elements for equality.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="list"></param>
-    /// <param name="comparer"></param>
-    /// <remarks>
-    /// <para>If you are using a built-in type you can specify the <see cref="Comparer{T}.Default"/></para>
-    /// <para>Do not use this method on unsorted lists, it will not work.</para>
-    /// <para>For unsorted you can use <see cref="SortAndRemoveDuplicates{T}(List{T}, IComparer{T})"/></para>
-    /// </remarks>
-    public static void RemoveDuplicatesSorted<T>(this List<T> list, IComparer<T> comparer) {
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="list">The list to remove duplicates from.</param>
+    /// <param name="isSorted">Whether the list is sorted. Default is false.</param>
+    /// <param name="comparer">An optional comparer to use for comparing elements for equality. Default is null.</param>
+    public static void RemoveDuplicates<T>(this List<T> list, bool isSorted = false, IEqualityComparer<T>? comparer = null) {
+        if (isSorted) {
+            list.RemoveDuplicatesSorted(comparer);
+            return;
+        }
+        list.RemoveDuplicatesNotSorted(comparer);
+    }
+
+    // Removes duplicates from a sorted list in 1 iteration
+    private static void RemoveDuplicatesSorted<T>(this List<T> list, IEqualityComparer<T>? comparer) {
         if (list is { Count: <= 1 }) {
             return;
         }
+        comparer ??= EqualityComparer<T>.Default;
         var current = list[0];
         int i = 1;
         while (i < list.Count) {
-            if (comparer.Compare(list[i], current) is 0) {
+            if (comparer.Equals(list[i], current)) {
                 list.RemoveAt(i);
                 continue;
             }
@@ -105,17 +118,48 @@ public static partial class Extensions {
         }
     }
 
+    // Removes duplicates from List using a HashSet in 1 iteration
+    private static void RemoveDuplicatesNotSorted<T>(this List<T> list, IEqualityComparer<T>? comparer) {
+        if (list is { Count: <= 1 }) {
+            return;
+        }
+        var hSet = new HashSet<T>(comparer);
+        int i = 0;
+        while (i < list.Count) {
+            if (hSet.Add(list[i])) {
+                i++;
+                continue;
+            }
+            list.RemoveAt(i);
+        }
+    }
+
     /// <summary>
-    /// Sorts and removes duplicates for a list
+    /// Splits the input array into a list of array segments of the specified size.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="list"></param>
-    /// <param name="comparer"></param>
+    /// <typeparam name="T">The type of the elements in the array.</typeparam>
+    /// <param name="arr">The input array to split.</param>
+    /// <param name="sizeOfChunk">The size of each chunk.</param>
+    /// <returns>A list of array segments of the specified size.</returns>
     /// <remarks>
-    /// If you are using a built-in type you can specify the <see cref="Comparer{T}.Default"/>
+    /// This is slightly less efficient than spans but ArraySegment is more flexible since it has less restrictions
     /// </remarks>
-    public static void SortAndRemoveDuplicates<T>(this List<T> list, IComparer<T> comparer) {
-        list.Sort(comparer);
-        list.RemoveDuplicatesSorted(comparer);
+    public static List<ArraySegment<T>> ChunkToSegments<T>(this T[] arr, int sizeOfChunk) {
+        Debug.Assert(sizeOfChunk > 0, "Size of chunk must be greater than 0");
+        var list = new List<ArraySegment<T>>();
+        if (arr.Length is 0) {
+            return list;
+        }
+        int i = 0;
+        while (i < arr.Length) {
+            var remaining = arr.Length - i;
+            if (remaining <= sizeOfChunk) {
+                list.Add(new ArraySegment<T>(arr, i, remaining));
+                break;
+            }
+            list.Add(new ArraySegment<T>(arr, i, sizeOfChunk));
+            i += sizeOfChunk;
+        }
+        return list;
     }
 }
