de1e43bbe3b1d33db1d13a18b67d7feca2a24d55
David Shnayder <dusrdev@gmail.com>
2023-11-22 10:23:46 +0200

    Added ICollection.IsNullOrEmpty

--- Sharpify/CollectionExtensions.cs
+++ Sharpify/CollectionExtensions.cs
@@ -7,6 +7,14 @@ namespace Sharpify;
 
 public static partial class Extensions {
     /// <summary>
+    /// Determines whether the specified collection is null or empty.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to check.</param>
+    /// <returns><c>true</c> if the collection is null or empty; otherwise, <c>false</c>.</returns>
+    public static bool IsNullOrEmpty<T>(this ICollection<T>? collection) => collection is null or { Count: 0 };
+
+    /// <summary>
     /// Returns the span of a list
     /// </summary>
     /// <typeparam name="T"></typeparam>
