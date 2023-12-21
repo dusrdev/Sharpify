7e45971823377f076d16388e1a61af2de6011cd3
David Shnayder <dusrdev@gmail.com>
2023-12-21 21:53:17 +0200

    Moved FormatBytes to String Utils

--- Sharpify/UnmanangedExtensions.cs
+++ Sharpify/UnmanangedExtensions.cs
@@ -3,35 +3,6 @@ using System.Runtime.CompilerServices;
 namespace Sharpify;
 
 public static partial class Extensions {
-    private static readonly ReadOnlyMemory<string> FileSizeSuffix = new(["B", "KB", "MB", "GB", "TB", "PB"]);
-
-    /// <summary>
-    /// Formats bytes to friendlier strings, i.e: B,KB,MB,TB,PB...
-    /// </summary>
-    /// <returns>string</returns>
-    public static string FormatBytes(this long bytes) => ((double)bytes).FormatBytes();
-
-    /// <summary>
-    /// Formats bytes to friendlier strings, i.e: B,KB,MB,TB,PB...
-    /// </summary>
-    /// <returns>string</returns>
-    [MethodImpl(MethodImplOptions.AggressiveOptimization | MethodImplOptions.NoInlining)]
-    public static string FormatBytes(this double bytes) {
-        const double kb = 1024d;
-        const double divisor = 1 / kb;
-        var suffix = 0;
-        while (bytes >= kb && suffix < FileSizeSuffix.Length) {
-            bytes *= divisor;
-            suffix++;
-        }
-        Span<char> buffer = stackalloc char[9];
-        Math.Round(bytes, 2).TryFormat(buffer, out var charsWritten);
-        buffer[charsWritten++] = ' ';
-        FileSizeSuffix.Span[suffix].CopyTo(buffer[charsWritten..]);
-        int len = suffix is 0 ? charsWritten + 1 : charsWritten + 2;
-        return new string(buffer[0..len]);
-    }
-
     /// <summary>
     /// Tries to parse an enum result from a string
     /// </summary>
---
+++ Sharpify/UtilsStrings.cs
@@ -0,0 +1,42 @@
+using System.Runtime.CompilerServices;
+
+namespace Sharpify;
+
+public static partial class Utils {
+    /// <summary>
+    /// Provides utility methods for <see cref="string"/>
+    /// </summary>
+    public static class Strings {
+        private static readonly ReadOnlyMemory<string> FileSizeSuffix = new(["B", "KB", "MB", "GB", "TB", "PB"]);
+
+        /// <summary>
+        /// Formats bytes to friendlier strings, i.e: B,KB,MB,TB,PB...
+        /// </summary>
+        /// <returns>string</returns>
+        public static string FormatBytes(long bytes) => FormatBytes((double)bytes);
+
+        /// <summary>
+        /// Formats bytes to friendlier strings, i.e: B,KB,MB,TB,PB...
+        /// </summary>
+        /// <returns>string</returns>
+        [MethodImpl(MethodImplOptions.AggressiveOptimization | MethodImplOptions.NoInlining)]
+        public static string FormatBytes(double bytes) {
+            const double kb = 1024d;
+            const double divisor = 1 / kb;
+            var suffix = 0;
+            while (bytes >= kb && suffix < FileSizeSuffix.Length) {
+                bytes *= divisor;
+                suffix++;
+            }
+            Span<char> buffer = stackalloc char[9];
+            int index = 0;
+            Math.Round(bytes, 2).TryFormat(buffer, out var charsWritten);
+            index += charsWritten;
+            buffer[index++] = ' ';
+            var suffixString = FileSizeSuffix.Span[suffix];
+            suffixString.CopyTo(buffer[index..]);
+            index += suffixString.Length;
+            return new string(buffer[0..index]);
+        }
+    }
+}
