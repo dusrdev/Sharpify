5c34cb8cdd5860806984b2709624ec80cdbb7608
David Shnayder <dusrdev@gmail.com>
2024-01-02 22:11:56 +0200

    Updated and fixed versions

--- Sharpify/AssemblyInfo.cs
+++ Sharpify/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyFileVersion("1.3.1")]
-[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.4.0")]
