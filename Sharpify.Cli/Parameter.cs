d88b1c454f91c10be86d721b674f505e9b086a90
David Shnayder <dusrdev@gmail.com>
2024-01-14 18:19:13 +0200

    Removed Cli

--- Sharpify.Cli/Parameter.cs
+++
@@ -1,9 +0,0 @@
-using System.Globalization;
-
-namespace Sharpify.Cli;
-
-public readonly record struct Parameter(string Name, string Description, bool IsRequired = true, string DefaultValue = "") {
-	public bool TryParse<T>(out T? val) where T : IParsable<T> {
-		return T.TryParse(DefaultValue, CultureInfo.InvariantCulture, out val);
-	}
-}
