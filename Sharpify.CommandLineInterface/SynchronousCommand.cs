5dfce162b07d59a32a26b9bf16d12a7e77e39176
David Shnayder <dusrdev@gmail.com>
2024-03-13 18:53:11 +0200

    Added sync command - an alternative to command

---
+++ Sharpify.CommandLineInterface/SynchronousCommand.cs
@@ -0,0 +1,21 @@
+namespace Sharpify.CommandLineInterface;
+
+/// <summary>
+/// An alternative to <see cref="Command"/> that runs synchronously.
+/// </summary>
+/// <remarks>
+/// This is syntactic sugar for writing wrapping returns from ExecuteAsync in ValueTask.FromResult
+/// </remarks>
+public abstract class SynchronousCommand : Command {
+	/// <inheritdoc/>
+	public override ValueTask<int> ExecuteAsync(Arguments args) {
+		return ValueTask.FromResult(Execute(args));
+	}
+
+	/// <summary>
+	/// Executes the command.
+	/// </summary>
+	/// <param name="args"></param>
+	/// <returns>Status code</returns>
+	public abstract int Execute(Arguments args);
+}
