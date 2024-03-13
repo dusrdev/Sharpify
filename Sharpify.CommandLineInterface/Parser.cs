c7b44a169f825963398f8f3145708d7f8c2b5da4
David Shnayder <dusrdev@gmail.com>
2024-03-13 18:52:17 +0200

    Updated parsing and fixed possible issues

--- Sharpify.CommandLineInterface/Parser.cs
+++ Sharpify.CommandLineInterface/Parser.cs
@@ -13,6 +13,7 @@ public static class Parser {
     /// </summary>
     /// <param name="str"></param>
     /// <param name="buffer">The buffer is used to reduce allocation, to overestimate, the length of str should suffice</param>
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public static ArraySegment<string> Split(ReadOnlySpan<char> str, string[] buffer) {
         if (str.Length is 0) {
             return ArraySegment<string>.Empty;
@@ -25,7 +26,7 @@ public static class Parser {
                 i++;
                 continue;
             }
-            if (c is '"') {
+            if (c is '"') { // everything without a quote block is a single item, regardless of spaces
                 str = str[(i + 1)..];
                 int nextQuote = str.IndexOf('"');
                 if (nextQuote is -1) {
@@ -35,9 +36,10 @@ public static class Parser {
                 i = nextQuote + 1;
                 continue;
             }
+            // next is a word
             str = str[i..];
             int nextSpace = str.IndexOf(' ');
-            if (nextSpace <= 0) {
+            if (nextSpace <= 0) { // the last word, no spaces after
                 buffer[pos++] = new string(str);
                 i = str.Length;
                 continue;
@@ -107,7 +109,7 @@ public static class Parser {
     public static Arguments? ParseArguments(ReadOnlySpan<string> args, StringComparer comparer) => ParseArgumentsInternal(args, comparer);
 
     // Parses a List<string> into a dictionary of arguments
-    private static Arguments? ParseArgumentsInternal(ReadOnlySpan<string> args, StringComparer comparer) {
+    internal static Arguments? ParseArgumentsInternal(ReadOnlySpan<string> args, StringComparer comparer) {
         if (args.Length is 0) {
             return null;
         }
@@ -115,44 +117,72 @@ public static class Parser {
         var argsCopy = GC.AllocateUninitializedArray<string>(args.Length);
         args.CopyTo(argsCopy.AsSpan());
 
-        var results = new Dictionary<string, string>(args.Length, comparer);
-        int i = 0;
-
-        //TODO: named arguments aren't recognized? add tests and fix
-
-        while (i < args.Length && !IsParameterName(args[i])) {
-            results[i.ToString()] = args[i];
-            i++;
-        }
-
-        while (i < args.Length) {
-            var current = args[i];
-            // Ignore string as it is invalid parameter name
-            if (!IsParameterName(current)) {
-                i++;
-                continue;
-            }
-            // Parameter name
-            int ii = 0;
-            while (current[ii] is '-') {
-                ii++;
-            }
-            // Next is unavailable or another parameter
-            if (i + 1 == args.Length || IsParameterName(args[i + 1])) {
-                results[current[ii..]] = string.Empty;
-                i++;
-                continue;
-            }
-            // Next is available and not a parameter but rather a value
-            results[current[ii..]] = args[i + 1];
-            i += 2;
-        }
+        var results = MapArguments(argsCopy, comparer);
 
         return results.Count is 0 ? null : new Arguments(argsCopy, results);
     }
 
+    // Maps a List of strings into a dictionary of arguments
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+    internal static Dictionary<string, string> MapArguments(ReadOnlySpan<string> args, StringComparer comparer) {
+        var results = new Dictionary<string, string>(args.Length, comparer);
+        Span<bool> mapped = stackalloc bool[args.Length];
+        int i = 0;
+
+        // Named arguments
+        while (i < args.Length) {
+            var current = args[i];
+            // This is positional argument, processed in the next loop
+            // values of named params are processed in the single iteration of the named parameter
+            if (!IsParameterName(current)) {
+                i++;
+                continue;
+            }
+            // This is parameter name (starts with either - or --)
+            int ii = 0;
+            while (current[ii] is '-') { // Skip the dashes
+                ii++;
+            }
+            var name = current[ii..]; // Parameter name without dashes
+
+            // i + 1 == args.Length => checks if the next argument is available
+            // if not, then this is a switch (i.e. a named boolean toggle)
+            // IsParameterName(args[i + 1]) => checks if the next argument is a parameter
+            // if it is, then again, this is a switch
+            if (i + 1 == args.Length || IsParameterName(args[i + 1])) {
+                results[name] = string.Empty;
+                mapped[i] = true;
+                i++;
+                continue;
+            }
+            // If the previous condition didn't take
+            // then this is the value of the named parameter
+            var value = args[i + 1];
+            results[name] = value;
+            mapped[i] = mapped[i + 1] = true;
+            i += 2;
+        }
+
+        int position = 0;
+
+        // Positional arguments (mapped as {pos: value})
+        // The positional arguments are mapped in the order they appear
+        // And the number of the positional argument
+        // A positional argument may have the key 0, even if it is the last enter argument (assuming other arguments are named or switches)
+        for (i = 0; i < args.Length; i++) {
+            if (mapped[i]) {
+                continue;
+            }
+            results[position.ToString()] = args[i];
+            position++;
+            mapped[i] = true;
+        }
+
+        return results;
+    }
+
     // Checks whether a string starts with "-"
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     private static bool IsParameterName(ReadOnlySpan<char> str) {
         return str.StartsWith("-");
     }
