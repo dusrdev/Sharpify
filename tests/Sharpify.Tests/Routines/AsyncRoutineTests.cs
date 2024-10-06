90f2a9caf8091b9142594edd566480985ac1aaab
David Shnayder <dusrdev@gmail.com>
2024-10-06 18:27:41 +0300

    Fix #2 for routines

--- tests/Sharpify.Tests/Routines/AsyncRoutineTests.cs
+++ tests/Sharpify.Tests/Routines/AsyncRoutineTests.cs
@@ -4,10 +4,10 @@ namespace Sharpify.Tests.Routines;
 
 public class AsyncRoutineTests {
     [Theory]
-    [InlineData(550, 5)]
-    [InlineData(150, 1)]
-    [InlineData(1050, 10)]
-    [InlineData(50, 0)]
+    [InlineData(600, 5)]
+    [InlineData(200, 1)]
+    [InlineData(1100, 10)]
+    [InlineData(100, 0)]
     public async Task AsyncRoutine_GivenIncreaseValueFunction_IncreasesValue(int milliseconds, int expected) {
         // Arrange
         var count = 0;
--- tests/Sharpify.Tests/Routines/RoutineTests.cs
+++ tests/Sharpify.Tests/Routines/RoutineTests.cs
@@ -4,10 +4,10 @@ namespace Sharpify.Tests.Routines;
 
 public class RoutineTests {
     [Theory]
-    [InlineData(550, 5)]
-    [InlineData(150, 1)]
-    [InlineData(1050, 10)]
-    [InlineData(50, 0)]
+    [InlineData(600, 5)]
+    [InlineData(200, 1)]
+    [InlineData(1100, 10)]
+    [InlineData(100, 0)]
     public async Task Routine_GivenIncreaseValueFunction_IncreasesValue(int milliseconds, int expected) {
         // Arrange
         var count = 0;
