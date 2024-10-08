8034dad30acfb748085cc8bd5d7cbfe4639296db
David Shnayder <dusrdev@gmail.com>
2024-10-08 16:10:16 +0300

    Removed delays from routine tests

--- tests/Sharpify.Tests/Routines/AsyncRoutineTests.cs
+++ tests/Sharpify.Tests/Routines/AsyncRoutineTests.cs
@@ -4,29 +4,28 @@ namespace Sharpify.Tests.Routines;
 
 public class AsyncRoutineTests {
     [Theory]
-    [InlineData(600, 5)]
-    [InlineData(200, 1)]
-    [InlineData(1100, 10)]
-    [InlineData(100, 0)]
-    public async Task AsyncRoutine_GivenIncreaseValueFunction_IncreasesValue(int milliseconds, int expected) {
+    [InlineData(5)]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(0)]
+    public async Task AsyncRoutine_GivenIncreaseValueFunction_IncreasesValue(int expected) {
         // Arrange
         var count = 0;
-        var tcs = new TaskCompletionSource<bool>();
+        var tcs = new TaskCompletionSource();
         var options = RoutineOptions.ExecuteInParallel;
-        using var routine = new AsyncRoutine(TimeSpan.FromMilliseconds(100))
+        using var routine = new AsyncRoutine(TimeSpan.FromMilliseconds(50))
                         .ChangeOptions(options)
                         .Add(_ => {
                             var newCount = Interlocked.Increment(ref count);
                             if (newCount >= expected) {
-                                tcs.TrySetResult(true);
+                                tcs.TrySetResult();
                             }
                             return Task.CompletedTask;
                         });
 
         // Act
         _ = routine.Start();
-        var delayTask = Task.Delay(milliseconds);
-        var completedTask = await Task.WhenAny(tcs.Task, delayTask);
+        await tcs.Task;
 
         // Assert
         count.Should().BeGreaterThanOrEqualTo(expected);
--- tests/Sharpify.Tests/Routines/RoutineTests.cs
+++ tests/Sharpify.Tests/Routines/RoutineTests.cs
@@ -4,25 +4,24 @@ namespace Sharpify.Tests.Routines;
 
 public class RoutineTests {
     [Theory]
-    [InlineData(600, 5)]
-    [InlineData(200, 1)]
-    [InlineData(1100, 10)]
-    [InlineData(100, 0)]
-    public async Task Routine_GivenIncreaseValueFunction_IncreasesValue(int milliseconds, int expected) {
+    [InlineData(5)]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(0)]
+    public async Task Routine_GivenIncreaseValueFunction_IncreasesValue(int expected) {
         // Arrange
         var count = 0;
-        var tcs = new TaskCompletionSource<bool>();
-        using var routine = new Routine(100).Add(() => {
+        var tcs = new TaskCompletionSource();
+        using var routine = new Routine(50).Add(() => {
             Interlocked.Increment(ref count);
             if (count >= expected) {
-                tcs.TrySetResult(true);
+                tcs.TrySetResult();
             }
         });
 
         // Act
         routine.Start();
-        var delayTask = Task.Delay(milliseconds);
-        var completedTask = await Task.WhenAny(tcs.Task, delayTask);
+        await tcs.Task;
 
         // Assert
         count.Should().BeGreaterThanOrEqualTo(expected);
