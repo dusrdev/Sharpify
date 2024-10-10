d081d743a011ddf7d8a0dcb75405a4ab12f94830
David Shnayder <dusrdev@gmail.com>
2024-10-10 10:07:50 +0300

    Expended test coverage

--- tests/Sharpify.CommandLineInterface.Tests/CliBuilderTests.cs
+++ tests/Sharpify.CommandLineInterface.Tests/CliBuilderTests.cs
@@ -42,6 +42,66 @@ public class CliBuilderTests {
 	}
 
 	[Fact]
+	public async Task Runner_WithCustomWriter_SingleCommand_HelpCommand_OutputsAllInfo() {
+		var single = new SingleCommand();
+		var writer = new StringWriter(new StringBuilder(), CultureInfo.CurrentCulture);
+
+		var cliRunner = CliRunner.CreateBuilder()
+                           .AddCommand(single)
+						   .SetOutputWriter(writer)
+						   .WithMetadata(options => {
+							options.Name = "Single";
+							options.Description = "A single command";
+							options.Version = "1.0.0";
+							options.Author = "David";
+							options.License = "MIT";
+						   })
+                           .Build();
+		await cliRunner.RunAsync("help", false);
+
+		var output = writer.ToString();
+		output.Should().Contain("Single");
+		output.Should().Contain("A single command");
+		output.Should().Contain("Version: 1.0.0");
+		output.Should().Contain("Author: David");
+		output.Should().Contain("License: MIT");
+		output.Should().Contain(single.Usage);
+	}
+
+	[Theory]
+	[InlineData("help")]
+	[InlineData("--help")]
+	public async Task Runner_WithCustomWriter_SingleCommand_HelpCommand_OutputsCommandUsageToWriter(string input) {
+		var single = new SingleCommand();
+		var writer = new StringWriter(new StringBuilder(), CultureInfo.CurrentCulture);
+
+		var cliRunner = CliRunner.CreateBuilder()
+                           .AddCommand(single)
+						   .SetOutputWriter(writer)
+                           .Build();
+		await cliRunner.RunAsync(input, false);
+
+		writer.ToString().Should().Contain(single.Usage);
+	}
+
+	[Theory]
+	[InlineData("version")]
+	[InlineData("--version")]
+	public async Task Runner_WithCustomWriter_SingleCommand_VersionCommand_OutputsVersionToWriter(string input) {
+		var single = new SingleCommand();
+		var writer = new StringWriter(new StringBuilder(), CultureInfo.CurrentCulture);
+
+		var cliRunner = CliRunner.CreateBuilder()
+                           .AddCommand(single)
+						   .SetOutputWriter(writer)
+						   .WithMetadata(options => options.Version = "1.0.0")
+                           .Build();
+		await cliRunner.RunAsync(input, false);
+
+		writer.ToString().Should().Contain("Version: 1.0.0");
+	}
+
+	[Fact]
 	public async Task Runner_WithCustomWriterMultipleCommands_OutputsGeneralHelpToWriter() {
 		var echo = new EchoCommand();
 		var add = new AddCommand();
@@ -59,7 +119,24 @@ public class CliBuilderTests {
 	}
 
 	[Fact]
-	public async Task Runner_WithCustomWriterAndMetadata_OutputsGeneralHelpToWriter() {
+	public async Task Runner_WithCustomWriterAndMetadata_HelpCommand_OutputsGeneralHelpToWriter() {
+		var echo = new EchoCommand();
+		var add = new AddCommand();
+		var writer = new StringWriter(new StringBuilder(), CultureInfo.CurrentCulture);
+
+		var cliRunner = CliRunner.CreateBuilder()
+                           .AddCommand(echo)
+						   .AddCommand(add)
+						   .SetOutputWriter(writer)
+						   .WithMetadata(options => options.Author = "Dave")
+                           .Build();
+		await cliRunner.RunAsync("help");
+
+		writer.ToString().Should().Contain("Dave");
+	}
+
+	[Fact]
+	public async Task Runner_WithCustomWriterAndMetadata_HelpFlag_OutputsGeneralHelpToWriter() {
 		var echo = new EchoCommand();
 		var add = new AddCommand();
 		var writer = new StringWriter(new StringBuilder(), CultureInfo.CurrentCulture);
@@ -73,6 +150,46 @@ public class CliBuilderTests {
 		await cliRunner.RunAsync("--help");
 
 		writer.ToString().Should().Contain("Dave");
+	}
+
+	[Fact]
+	public async Task Runner_WithCustomWriterAndMetadata_VersionCommand_OutputsVersionToWriter() {
+		var echo = new EchoCommand();
+		var add = new AddCommand();
+		var writer = new StringWriter(new StringBuilder(), CultureInfo.CurrentCulture);
+
+		var cliRunner = CliRunner.CreateBuilder()
+                           .AddCommand(echo)
+						   .AddCommand(add)
+						   .SetOutputWriter(writer)
+						   .WithMetadata(options => {
+                               options.Author = "Dave";
+							   options.Version = "1.0.0";
+                           })
+                           .Build();
+		await cliRunner.RunAsync("version");
+
+		writer.ToString().Should().Contain("Version: 1.0.0");
+	}
+
+	[Fact]
+	public async Task Runner_WithCustomWriterAndMetadata_VersionFlag_OutputsVersionToWriter() {
+		var echo = new EchoCommand();
+		var add = new AddCommand();
+		var writer = new StringWriter(new StringBuilder(), CultureInfo.CurrentCulture);
+
+		var cliRunner = CliRunner.CreateBuilder()
+                           .AddCommand(echo)
+						   .AddCommand(add)
+						   .SetOutputWriter(writer)
+						   .WithMetadata(options => {
+                               options.Author = "Dave";
+							   options.Version = "1.0.0";
+                           })
+                           .Build();
+		await cliRunner.RunAsync("--version");
+
+		writer.ToString().Should().Contain("Version: 1.0.0");
 	}
 
 	[Fact]
