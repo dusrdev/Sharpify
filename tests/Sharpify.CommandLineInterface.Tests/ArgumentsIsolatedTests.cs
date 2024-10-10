6d5ac3374c99f92e75ade70f7f53c492ad1f342e
David Shnayder <dusrdev@gmail.com>
2024-10-10 09:55:56 +0300

    Fixed test naming

--- tests/Sharpify.CommandLineInterface.Tests/ArgumentsIsolatedTests.cs
+++ tests/Sharpify.CommandLineInterface.Tests/ArgumentsIsolatedTests.cs
@@ -4,28 +4,28 @@ namespace Sharpify.CommandLineInterface.Tests;
 /// This test class specifically tests that arguments are parsed correctly, and more importantly non-positional arguments are not lost during forwarding (Which happens with the cli builder naturally)
 /// </summary>
 public class ArgumentsIsolatedTests {
-	private static readonly Arguments _args = Parser.ParseArguments(
+	private static readonly Arguments Args = Parser.ParseArguments(
 		"command positional --named1 Harold --named2 Finch positional2 --flag --words \"word1|word2\" --numbers \"1|2\"")!;
-    private static readonly int[] expectation = [1, 2];
+    private static readonly int[] Expectation = [1, 2];
 
     [Fact]
 	public void Positional_BeforeForwarding_ParsedCorrectly() {
 		// Positional 0 [Command]
-		_args.TryGetValue(0, out var pos0).Should().BeTrue();
+		Args.TryGetValue(0, out var pos0).Should().BeTrue();
 		pos0.Should().Be("command");
 
 		// Positional 0 [positional]
-		_args.TryGetValue(1, out var pos1).Should().BeTrue();
+		Args.TryGetValue(1, out var pos1).Should().BeTrue();
 		pos1.Should().Be("positional");
 
 		// Positional 0 [positional2]
-		_args.TryGetValue(2, out var pos2).Should().BeTrue();
+		Args.TryGetValue(2, out var pos2).Should().BeTrue();
 		pos2.Should().Be("positional2");
 	}
 
 	[Fact]
 	public void Positional_AfterForwarding_ParsedCorrectly() {
-		var forwarded = _args.ForwardPositionalArguments();
+		var forwarded = Args.ForwardPositionalArguments();
 
 		// "command" no longer exists
 		// positional should be at 0, and positional2 at 1.
@@ -44,16 +44,16 @@ public class ArgumentsIsolatedTests {
 		// named1 - Harold
 		// named2 - Finch
 
-		_args.TryGetValue("named1", out var firstName).Should().BeTrue();
+		Args.TryGetValue("named1", out var firstName).Should().BeTrue();
 		firstName.Should().Be("Harold");
 
-		_args.TryGetValue("named2", out var lastName).Should().BeTrue();
+		Args.TryGetValue("named2", out var lastName).Should().BeTrue();
 		lastName.Should().Be("Finch");
 	}
 
 	[Fact]
 	public void Named_AfterForwarding_ParsedCorrectly() {
-		var forwarded = _args.ForwardPositionalArguments();
+		var forwarded = Args.ForwardPositionalArguments();
 
 		// named1 - Harold
 		// named2 - Finch
@@ -67,24 +67,24 @@ public class ArgumentsIsolatedTests {
 
 	[Fact]
 	public void Flag_BeforeForwarding_ParsedCorrectly() {
-		_args.HasFlag("flag").Should().BeTrue();
+		Args.HasFlag("flag").Should().BeTrue();
 	}
 
 	[Fact]
 	public void Flag_AfterForwarding_ParsedCorrectly() {
-		var forwarded = _args.ForwardPositionalArguments();
+		var forwarded = Args.ForwardPositionalArguments();
 		forwarded.HasFlag("flag").Should().BeTrue();
 	}
 
 	[Fact]
 	public void Named_Array_String_ParsedCorrectly() {
-		_args.TryGetValues("words", "|", out var words).Should().BeTrue();
+		Args.TryGetValues("words", "|", out var words).Should().BeTrue();
 		words.Should().BeEquivalentTo(["word1", "word2"]);
 	}
 
 	[Fact]
 	public void Named_Array_Int_ParsedCorrectly() {
-		_args.TryGetValues<int>("numbers", "|", out var numbers).Should().BeTrue();
-		numbers.Should().BeEquivalentTo(expectation);
+		Args.TryGetValues<int>("numbers", "|", out var numbers).Should().BeTrue();
+		numbers.Should().BeEquivalentTo(Expectation);
 	}
 }
