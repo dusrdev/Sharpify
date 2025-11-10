fd585b223eb6cd2078167c2cff61822f53c81e47
David Shnayder <dusrdev@gmail.com>
2025-11-10 11:34:31 +0200

    Remove sub-project which are no longer relevant in this repo

--- tests/Sharpify.Data.Tests/FactoryResult.cs
+++
@@ -1,5 +0,0 @@
-namespace Sharpify.Data.Tests;
-
-public record FactoryResult<T>(string Path, T Database) : IDisposable where T : IDisposable {
-    public void Dispose() => Database.Dispose();
-}
--- tests/Sharpify.Data.Tests/Person.cs
+++
@@ -1,6 +0,0 @@
-using MemoryPack;
-
-namespace Sharpify.Data.Tests;
-
-[MemoryPackable]
-public readonly partial record struct Person(string Name, int Age);
